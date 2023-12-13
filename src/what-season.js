2775189241161cecd3cf97cb167bebb6242750ac
Olga Krukova <krukovaolga1@gmail.com>
2023-12-13 23:23:44 +0200

    feat: solve what-season, dream-team, count-cats, hanoi-tower

--- src/what-season.js
+++ src/what-season.js
@@ -2,18 +2,39 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  let seasons = {
+    0: 'winter',
+    1: 'winter',
+    11: 'winter',
+    2: 'spring',
+    3: 'spring',
+    4: 'spring',
+    5: 'summer',
+    6: 'summer',
+    7: 'summer',
+    8: 'autumn',
+    9: 'autumn',
+    10: 'autumn'
+  }
+  try {
+    if (arguments.length === 0) return 'Unable to determine the time of year!';
+    if (date instanceof Date) {
+      date.setTime(date.getTime())
+    }
+    return seasons[date.getMonth()];
+  } catch(e){
+    throw new Error('Invalid date!');
+}
 }
 
 module.exports = {
