3711385fe881cb1834496f82d2602b14a7596329
Olga Krukova <krukovaolga1@gmail.com>
2023-12-15 23:45:16 +0200

    feat: solve minesweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [];
+  let arr;
+    for (let i = 0; i < matrix.length; i++){
+      arr = [];
+      for (let j = 0; j < matrix[0].length; j++){
+      let a11 = (i - 1 >= 0) && (j - 1 >= 0) ? matrix[i - 1][j - 1] : 0;
+      let a12 = (i - 1) >= 0 ? +matrix[i - 1][j] : 0;
+      let a13 = (i - 1 >= 0) && (j + 1 < matrix[0].length) ? +matrix[i - 1][j + 1] : 0;
+      let a21 = (j - 1 >=0) ? +matrix[i][j - 1] : 0;
+      let a23 = (j + 1 < matrix[0].length) ? +matrix[i][j + 1] : 0;
+      let a31 = (j - 1 >= 0) && (i + 1 < matrix.length) ? +matrix[i+1][j-1] : 0;
+      let a32 = (i + 1 < matrix.length) ? +matrix[i+1][j] : 0;
+      let a33 = (j + 1 < matrix[0].length) && (i + 1 < matrix.length) ? +matrix[i+1][j+1] : 0;
+
+      arr.push(0 + a11 + a12 + a13 + a21 + a23 + a31 + a32 + a33);
+      };
+    result.push(arr);
+  }
+  return result;
 }
 
 module.exports = {
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,30 +3,66 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direct) {
+  /*   try {
+      if (arguments.length === 0) throw new Error();
+      } catch(e) {
+      throw e;
+    } */
+    this.latin = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(str, key) {
+    try {
+      if (arguments.length === 0) throw new Error();
+    } catch (e) {
+      throw e;
+    }
+    let strToEncrypt = str.toUpperCase();
+    let keyWord = key.toUpperCase();
+    let result = [];
+    let pos;
+    let index;
+    let keyLength = keyWord.length;
+    let keyIndex = 0;
+    for (let i = 0; i < strToEncrypt.length; i++){
+      pos = this.latin.indexOf(strToEncrypt[i]);
+      if (pos === -1) {
+        result.push(strToEncrypt[i]);
+      } else {
+        index = this.latin.indexOf(keyWord[keyIndex%keyLength]) * 2;
+        index = index < this.latin.length ? index : index - this.latin.length;
+        result.push(this.latin[index]);
+        keyIndex++;
+      }
+    }
+    return result.join('');
+  }
+
+  decrypt(str, key) {
+    try {
+      if (arguments.length === 0) throw new Error();
+    } catch (e) {
+      throw e;
+    }
   }
 }
 
